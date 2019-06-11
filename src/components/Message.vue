d973979d5f267e06b3027ad38f79eb5ada7c7c9f
henser <hserro@gmail.com>
2019-06-11 12:48:06 +0100

    Added Message and MessagePlaceholder to project

---
+++ src/components/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-card class="card-message mb-4 border-0 shadow-sm text-left">
+    <b-card-body class="d-flex flex-column">
+      <b-card-text class="d-flex flex-column justify-content-between h-100">
+        <p class="text-muted">{{ item.author }}</p>
+        <p>{{ item.message }}</p>
+        <small class="text-muted">{{ item.timestamp | moment("DD MMM YYYY HH:mm") }}</small>
+      </b-card-text>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import moment from 'vue-moment'
+
+Vue.use(moment)
+
+export default {
+  name: 'Message',
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+  .card-message {
+    width: 90%;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .card-message {
+      width: 66%;
+    }
+  }
+</style>
---
+++ src/components/MessagePlaceholder.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="col-12 d-flex h-auto">
+    <b-card class="card-placeholder mb-4 border-0 shadow-sm mx-4 text-left">
+      <b-card-footer class="bg-white border-0">
+        <p class="card-placeholder__content w-50"></p>
+        <p class="card-placeholder__content w-100"></p>
+        <p class="card-placeholder__content w-25"></p>
+      </b-card-footer>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessagePlaceholder'
+}
+</script>
+
+<style scoped>
+
+.card-placeholder__content {
+  display: block;
+  border-radius: none;
+  animation: pulse 1s infinite ease-in-out;
+  height: 20px;
+  margin: 10px 0;
+}
+
+.card-placeholder {
+  width: 80%;
+}
+
+@media only screen and (min-width: 768px) {
+  .card-placeholder {
+    width: 50%;
+  }
+}
+
+@keyframes pulse {
+    0% {
+        background-color: rgba(165, 165, 165, 0.1);
+    }
+    50% {
+        background-color: rgba(165, 165, 165, 0.3);
+    }
+    100% {
+        background-color: rgba(165, 165, 165, 0.1);
+    }
+}
+</style>
